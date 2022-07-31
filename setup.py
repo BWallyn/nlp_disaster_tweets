fe6ba5fff97c60b006dcdeb7a353d58e103fe149
Benjamin WALLYN <38312538+BWallyn@users.noreply.github.com>
2022-07-31 17:22:10 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='model to predict which tweets are about real disasters and which one's aren't',
+    author='Benjamin Wallyn',
+    license='MIT',
+)
